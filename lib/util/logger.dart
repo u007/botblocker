76a3db10835a72dcab7244ab2db4b4cd33147f12
james <c00lways@gmail.com>
2019-11-27 11:20:42 +0800

    allow external logging

--- lib/util/logger.dart
+++ lib/util/logger.dart
@@ -1,3 +1,11 @@
 import 'package:logger/logger.dart';
 
-final logger = Logger();
+final logger = Logger(printer: LogHandler());
+
+class LogHandler extends LogPrinter {
+  @override
+  void log(Level level, message, error, StackTrace stackTrace) {
+    //May allow extend to provide external logging
+    println(message);
+  }
+}
