bc1a3cc75deea7ada07ba69a6cc157949d5fe8fb
james <c00lways@gmail.com>
2019-11-27 11:19:08 +0800

    fixed syntax
    building watcher

--- lib/sniffer.dart
+++ lib/sniffer.dart
@@ -1,23 +1,27 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
+import 'util/logger.dart';
 
+/// read log file from last line, otherwise from beginnning and find for bad url access
+/// and allow whitelisting of ip avoidance and also blacklist ip when needed via csf
 Future sniffLog(String logPath) async {
   if (FileSystemEntity.typeSync(logPath) == FileSystemEntityType.notFound) {
     throw ("${logPath} missing");
   }
 
+  //TODO read from last line
   var file = File(logPath);
   // Read file
-  var contents = StringBuffer();
+  // var contents = StringBuffer();
   var contentStream = file.openRead();
 
   await contentStream.transform(Utf8Decoder()).transform(LineSplitter()).listen(
-      (String line) = {
-        contents.write(line), // Add line to our StringBuffer object
-      },
-          
-      onDone: () => print(contents
-          .toString()), // Call toString() method to receive the complete data
-      onError: (e) => print('[Problems]: $e'));
+      (String line) {
+    logger.d("reading line ${line}");
+  }, onDone: () {
+    logger.i("completed ${logPath}");
+  }, onError: (e) {
+    logger.e("Error: ${e.toString()}");
+  });
 }
--- lib/watcher.dart
+++ lib/watcher.dart
@@ -1,2 +1,3 @@
 ///TODO make a watcher for all domain logs in a directory
 ///
+// watch /etc/apache2/logs/domlogs/*
