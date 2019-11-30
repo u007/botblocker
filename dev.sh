b910078e2094e690e41671ff6907eddf02890c97
james <c00lways@gmail.com>
2019-11-30 14:50:36 +0800

    wip parsing regex

---
+++ dev.sh
@@ -0,0 +1,6 @@
+
+sleep 1 && touch logs/webapplicationz.com &
+dart bin/bb.dart
+# export LASTPID=$!
+# echo $LASTPID
+# dart bin/bb.dart & ()
--- lib/sniffer.dart
+++ lib/sniffer.dart
@@ -4,10 +4,15 @@ import 'dart:convert';
 import 'util/logging.dart';
 
 RegExp matchLogLine = new RegExp(
-  r"([^ ])*\s+([^ ])*\s+([^ ]*)\s+([^ ]*)\s+([^ ]*)).*",
+  // r"([^ ])*\S+([^ ])*\S+([^ ]*)\S+([^ ]*)\S+([^ ]*).*",
+  r"([^ ]*)\S+([^ ]*)\S+([^ ]*)\S+.*", //\S+([^ ]*)
   caseSensitive: false,
   multiLine: false,
 );
+
+const sensitiveCountLimit = [
+  {'text': '', 'triggerCount': 3}
+];
 
 /// read log file from last line, otherwise from beginnning and find for bad url access
 /// and allow whitelisting of ip avoidance and also blacklist ip when needed via csf
@@ -25,15 +30,39 @@ Future sniffLog(String logPath) async {
   await contentStream.transform(Utf8Decoder()).transform(LineSplitter()).listen(
       (String line) {
     logger.fine("read:$lineNo: $line");
-
     Iterable<RegExpMatch> words = matchLogLine.allMatches(line);
 
+    if (words == null) {
+      logger.severe("Found null!");
+      return;
+    }
     // List<String> words = line.split(new RegExp(r"[^ ]*\s+[^ ]*"));
 
-    logger.fine("words: ${words.length}: $words");
+    String ip, logDate = '';
+    int index = 0;
+    for (var match in words) {
+      if (match == null) {
+        index += 1;
+        continue;
+      }
+      logger.fine("match: ${match.groupCount} | (${match.group(1)})");
+
+      switch (index) {
+        case 0:
+          ip = match.group(1);
+          break;
+        case 3:
+          logDate = match.group(1);
+          break;
+      }
+      logger.fine("ip: $ip, date: $logDate");
+      index += 1;
+    }
+
+    // logger.fine("words: ${words.length}");
   }, onDone: () {
     logger.info("completed $logPath");
   }, onError: (e) {
-    logger.severe("Error: $e.toString()");
+    logger.severe("Error: ${e.toString()}");
   });
 }
