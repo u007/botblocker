8d854338555f8fe215cc53611517283d97cb96e0
james <c00lways@gmail.com>
2020-01-24 00:02:47 +0800

    store history with url

--- lib/store/file.dart
+++ lib/store/file.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
 
+const int defaultMaxLog = 50;
+const Duration defaultMaxDurationOfLog = Duration(hours: 3);
+
 class AccessClientHandler {
   Map<String, dynamic> logs;
   String ip;
@@ -10,19 +13,77 @@ class AccessClientHandler {
       {String this.configPath = ""}) {
     loadConfig();
   }
-  /// store count of access
-  addAccessCount(String file, String url, { int count = 1}) {
+  /// store count of access, returns the entry with:
+  /// 'history' List<Map<String, dynamic>>'
+  /// 'count' int
+  addAccessCount(String file, String url, { DateTime accessTime, int count = 1}) {
     if(! logs.containsKey(file)) {
-      this.resetAccessCount(file);
+      this.resetAccessCount(file, url: url);
     }
-    logs[file]['count'] += 1;
-    return logs[file];
+    logs[file][url]['history'].add({ 'time': accessTime ?? new DateTime.now() });
+    logs[file][url]['count'] += 1;
+    return logs[file][url];
   }
 
-  resetAccessCount(String file) {
-    logs[file] = { 'count': 0 };
+  /// reset access
+  resetAccessCount(String file, {String url: ''}) {
+    List<Map<String,dynamic>> history = [];
+    logs[file] = { };
+    if (url.isNotEmpty) {
+      logs[file][url] = { 'count': 0, 'history': history };
+    }
   }
 
+  // trim access to reduce memory usage
+  trim({String file: '', Duration maxDuration: defaultMaxDurationOfLog}) {
+    /// based on 3h log only
+    
+    DateTime now = DateTime.now();
+    DateTime fromDate = now.subtract(maxDuration);
+    if (file.isNotEmpty) {
+      logs[file].forEach((url, _) {
+        //clean by date
+        while(logs[file][url]['history'].length > 0) {
+          String date = logs[file][url]['history'][0]['time'];
+          DateTime tDate = DateTime.parse(date);
+          if (tDate.isBefore(fromDate)){
+            print("removing history $tDate");
+            logs[file][url]['history'].removeAt(0);
+          } else {
+            break;
+          }
+        }// reduce by time
+        while (logs[file][url]['history'].length > defaultMaxLog) {
+          logs[file][url]['history'].removeAt(0);
+        }// reduce by length
+        logs[file][url]['count'] = logs[file][url]['history'].length;
+      });//each url
+      return;
+    }
+
+    //trim all files
+    logs.forEach((String file, _) {
+      logs[file].forEach((url, _) {
+        //clean by date
+        while(logs[file][url]['history'].length > 0) {
+          String date = logs[file][url]['history'][0]['time'];
+          DateTime tDate = DateTime.parse(date);
+          if (tDate.isBefore(fromDate)){
+            print("removing history $tDate");
+            logs[file][url]['history'].removeAt(0);
+          } else {
+            break;
+          }
+        }// reduce by time
+        while (logs[file][url]['history'].length > defaultMaxLog) {
+          logs[file][url]['history'].removeAt(0);
+        }// reduce by length
+        logs[file][url]['count'] = logs[file][url]['history'].length;
+      });//each url
+    });
+  }
+
+  //persists log
   save() async {
     Map<String, dynamic> res = {
       'log': logs,
