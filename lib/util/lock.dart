8df0240790d41078bcf5e84da308e8acc23fa4c3
james <c00lways@gmail.com>
2020-08-04 02:49:41 +0800

    wip lockfile to allow only 1 log file processing

---
+++ lib/util/lock.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+import 'package:botblocker/util/logging.dart';
+
+const Duration defaultTimeout = Duration(minutes: 30);
+typedef requestHandler(dynamic req);
+
+class SingularProcess {
+  final String lockName;
+  final requestHandler handler;
+  File lockFile;
+  String path;
+  Duration timeout;
+  bool setupDone = false;
+
+  SingularProcess(this.lockName, this.handler, {lockDuration: defaultTimeout}) {
+    timeout = lockDuration;
+    path = "./${lockName}.lck";
+    lockFile = new File(path);
+    setup();
+  }
+
+  setup() async {
+    if (FileSystemEntity.typeSync(path) != FileSystemEntityType.notFound) {
+      await lockFile.deleteSync();
+    }
+  }
+
+  handle(dynamic p) async {
+    while (!setupDone) {
+      await sleep(Duration(milliseconds: 100));
+    }
+    logger.info("singularProcess($lockName) timeout $timeout");
+    var end = new DateTime.now().toUtc().add(timeout);
+    var now = new DateTime.now().toUtc();
+    var fileType = FileSystemEntity.typeSync(path);
+    while (fileType != FileSystemEntityType.notFound && now.isBefore(end)) {
+      await sleep(Duration(milliseconds: 100));
+      now = new DateTime.now().toUtc();
+      logger.fine("singularProcess($lockName) Waiting for lock ${path}");
+    }
+
+    if (fileType != FileSystemEntityType.notFound) {
+      throw "Unable to obtain lock ${path}";
+    }
+
+    logger.info("typeSync $fileType ok?");
+    logger.info("singularProcess($lockName) creating lockfile ${path}");
+    await lockFile.create(recursive: true);
+    // gwpLockSync.lockSync(FileLock.exclusive);
+    try {
+      return await handler(p);
+    } catch (err) {
+      logger.info("singularProcess($lockName) error: $err");
+      rethrow;
+    } finally {
+      if (FileSystemEntity.typeSync(path) != FileSystemEntityType.notFound) {
+        await lockFile.deleteSync();
+      }
+      // gwpLockSync.unlockSync();
+    }
+  }
+}
--- lib/watcher.dart
+++ lib/watcher.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:botblocker/util/lock.dart';
 import 'package:watcher/watcher.dart';
 import 'package:path/path.dart' as p;
 import 'util/logging.dart';
@@ -7,15 +10,18 @@ import './sniffer/file.dart';
 import './sniffer.dart';
 import 'package:mutex/mutex.dart';
 
+Map<String, bool> waitingFile = {};
+Mutex lock = Mutex();
+File lockFile = File(".watcher.lock");
+
 /// make a watcher for all domain logs in a directory
-///
 // watch /etc/apache2/logs/domlogs/*
 watchDestination(String path) async {
-  Mutex lock = Mutex();
   String absolutePath = p.absolute(path);
   logger.info("watching2 $absolutePath");
   var watcher = DirectoryWatcher(absolutePath);
-  watcher.events.listen((event) async {
+  var started = false;
+  watcher.events.listen(SingularProcess('watcher', (event) async {
     logger.fine("path: $path event: ${event.toString()}");
     final eventPath = event.path;
     if (eventPath.endsWith('~') ||
@@ -28,17 +34,42 @@ watchDestination(String path) async {
       return;
     }
 
+    // await lock.acquire();
+    if (!waitingFile.containsKey(eventPath)) {
+      waitingFile[eventPath] = true;
+      logger.info("aded queue: path: $eventPath event: ${event.toString()}");
+      await processQueue();
+    } else {
+      logger.info(
+          "skip exists queue: path: $eventPath event: ${event.toString()}");
+    }
+    // await lock.release();
     //if file pattern
+  }).handler);
+}
+
+processQueue() async {
+  logger.fine("processing queue ${waitingFile.keys}");
+  // await lock.acquire();
+  while (waitingFile.keys.length > 0) {
+    final pendingFile = waitingFile.keys.first;
+
     try {
-      await lock.acquire();
-      await sniffLog(eventPath, FileSnifferHandler());
+      await sniffLog(pendingFile, FileSnifferHandler());
+      waitingFile.remove(pendingFile);
     } on FileSystemException {
       //ignore
-      logger.fine("ignoring missing $eventPath");
+      logger.fine("ignoring missing $pendingFile");
     } catch (error, stack) {
-      logger.info("path: $eventPath unhandled exception: $error | $stack");
-    } finally {
-      lock.release();
+      logger.info("path: $pendingFile unhandled exception: $error | $stack");
     }
-  });
+  }
+
+  // await lock.release();
+
+  // Timer.run(() async {
+  //   logger.fine("loop... sleeping 5s");
+  //   await sleep(Duration(seconds: 5));
+  //   processQueue();
+  // });
 }
